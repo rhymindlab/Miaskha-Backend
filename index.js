11bdc9f7d2e41bb08a2c8fe58f33d9fc66c0fadb
rhymindlab <rhymind@outlook.in>
2026-07-21 17:30:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.static("public"));
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://www.miashka.com/"
+        "https://www.miashka.com"
     ],
     credentials: true
 }));

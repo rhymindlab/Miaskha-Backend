fc48ee10c64367493db64eaaa384cef55cdd9de2
Akash Yadav <akashy9810@gmail.com>
2026-07-23 11:50:30 +0530

    Added Some Bug Fixes

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-const {
-    createOrder,
-    verifyPayment,
-    paymentFailed,
-    razorpayWebhook
-} = require("../controllers/paymentController");
+// const {
+//     createOrder,
+//     verifyPayment,
+//     paymentFailed,
+//     razorpayWebhook
+// } = require("../controllers/paymentController");
 
-const auth = require("../middleware/auth");
+// const auth = require("../middleware/auth");
 
-// Create Razorpay Order
-router.post("/create-order", auth, createOrder);
+// // Create Razorpay Order
+// router.post("/create-order", auth, createOrder);
 
-// Verify Payment
-router.post("/verify", auth, verifyPayment);
+// // Verify Payment
+// router.post("/verify", auth, verifyPayment);
 
-// Payment Failed
-router.post("/failed", auth, paymentFailed);
+// // Payment Failed
+// router.post("/failed", auth, paymentFailed);
 
-// Razorpay Webhook
-router.post(
-    "/webhook",
-    express.raw({ type: "application/json" }),
-    razorpayWebhook
-);
+// // Razorpay Webhook
+// router.post(
+//     "/webhook",
+//     express.raw({ type: "application/json" }),
+//     razorpayWebhook
+// );
 
-module.exports = router;
+// module.exports = router;
